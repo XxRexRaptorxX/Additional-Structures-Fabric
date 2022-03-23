f495b1e2bae591a4863c846fa249e4006dd72296
XxRexRaptorxX <Raptor-Simon@web.de>
2022-03-23 12:30:43 +0100

    Initial commit

---
+++ src/main/java/xxrexraptorxx/additionalstructures/AdditionalStructures.java
@@ -0,0 +1,15 @@
+package xxrexraptorxx.additionalstructures;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AdditionalStructures implements ModInitializer {
+
+	public static final Logger LOGGER = LoggerFactory.getLogger("additionalstructures");
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
