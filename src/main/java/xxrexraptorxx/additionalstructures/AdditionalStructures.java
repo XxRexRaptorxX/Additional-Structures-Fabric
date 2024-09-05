ef9185e56dce1fa4b829fe12791f465b1f5c5b94
XxRexRaptorxX <Raptor-Simon@web.de>
2024-09-05 22:23:48 +0200

    datapack changes

--- src/main/java/xxrexraptorxx/additionalstructures/AdditionalStructures.java
+++ src/main/java/xxrexraptorxx/additionalstructures/AdditionalStructures.java
@@ -4,12 +4,16 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * @author XxRexRaptorxX (RexRaptor)
+ * @projectPage <a href="https://www.curseforge.com/minecraft/mc-mods/additional-structures-fabric">...</a>
+ **/
 public class AdditionalStructures implements ModInitializer {
 
 	public static final Logger LOGGER = LoggerFactory.getLogger("additionalstructures");
 
 	@Override
 	public void onInitialize() {
-
 	}
+
 }
